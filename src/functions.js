761c96ec44f65c5ea37f9a218ab30f0c11d4bf9d
dwipu91 <aktruzzamandwiu@gmail.com>
2023-07-13 22:54:06 +0600

    facebook time line

---
+++ src/functions.js
@@ -0,0 +1,162 @@
+// const peoplePost = [
+    
+//     {
+// 		id: 1,
+// 		post_content: `যে ব্যক্তি তোমার যত্ন নেয়, কখনো তার কষ্টের কারণ হয়ো না। 
+// 		সাবিত রায়হান।`,
+// 		post_media: {
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/348275840_1300753333845049_3853926130820297252_n.jpg?stp=dst-jpg_s640x640&_nc_cat=106&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeFrBTDDDdg6Kb5e05cRlgMs6t1Y1HWpgPvq3VjUdamA--hj-0k23fklWHSt4G0mTEPmssjDswRCVJrhXFlS-I60&_nc_ohc=dsyUq_mFW58AX8elbmO&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfAzRtvfKa_kyBIJAQRWJxNwNB5GPYYjuz5dSD7ilrQ32w&oe=64A4DF0B",
+// 		},
+
+// 		like_count: 10000,
+// 		comment_count: 2000,
+// 		post_time: "5 day",
+// 		user: {
+// 			name: "Sabit International",
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/320218982_903784237720255_7043190058940161294_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGFTkZ9s6gIKckGXuTXhfR4_ePyTP0Oq3T94_JM_Q6rdG5KiOC5X5qbWwGF_4UqxVDSq_0nFQVIpPECse6Gm7HO&_nc_ohc=oAq79Wj8LKQAX9U5-ZL&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfAdXAkfNQ3XUtUFuQ6y5XSABawOOTUGPZe4-yb2MyUZ-w&oe=64A55E0E",
+// 		},
+// 	},
+// 	{
+// 		id: 2,
+// 		post_content: `দর্শক চাহিদার কারণে আগামীকাল "স্টার সিনেপ্লেক্স-বসুন্ধরা শপিং কমপ্লেক্স" এ 'সুরঙ্গ' সিনেমা ২টি শো, "এসকেএস টাওয়ার-ঢাকা" শাখায় ৩টি শো, "সনি স্কয়ার-মিরপুর" শাখায় ২টি শো, "সিমান্ত সম্ভার-ঢাকা" শাখায় ২টি শো বাড়ানো হয়েছে। সবমিলিয়ে স্টার সিনেপ্লেক্সের ৬টি ব্রাঞ্চে আগামীকাল ২৭টি শো চলবে 'সুরঙ্গ' সিনেমার👏`,
+// 		post_media: {
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/356987380_671339215009660_627280754634498213_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGJ-OcNIk9ayxAO2Z_aZJ6HFlG0ws9z5bkWUbTCz3Pluep4T4TjhIRH1XwnfPIpjkG19xFgia9Dsg3QMMsJZECS&_nc_ohc=TzW-_iVLzZ4AX_b8zZG&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfCrAQvcjGuBSTaCGjEqHGIc6_NiS4u-lZ1saFZHRdlIOw&oe=64A4A0A7",
+// 		},
+
+// 		like_count: 6400,
+// 		comment_count: 500,
+// 		post_time: " 3 day",
+// 		user: {
+// 			name: "BMR - Bangla Movie Review",
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/309287274_475089191301331_2499876996184979508_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFmOUnsjbmLUJTQkvx3_N-aGab_ErZ0n6QZpv8StnSfpPsOXUVS7_RXkO7JWEooKGz6DY_BdIsahAL2JldbSwuy&_nc_ohc=OamAb1GPW4MAX8mDLoV&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfALdLHk3vNS9mKiQZ5tYvC-0t6Rpc_1EJepN4gcWFkoqw&oe=64A60E33",
+// 		},
+// 	},
+//     {
+// 		id: 3,
+// 		post_content: `ব্যাটার তাসকিনের প্রশংসায় মেতেছেন টাইগারদের সহকারী কোচ নিক পোথাস!
+
+// 		পোথাস মনে করেন, আধুনিক ক্রিকেটে দলের প্রয়োজনে ইনিংসের শেষেরদিকে ব্যাট হাতে দলের হাল ধরতে হয় বোলারদের। তাদের থেকে ছোট ছোট ক্যামিওগুলো দলের স্কোর বাড়াতে গুরুত্বপূর্ণ ভূমিকা হয়ে উঠে। অথচ এ জায়গাটাতেই এখনো পিছিয়ে বাংলাদেশ। তবে আগের তুলনায় কিছুটা হলেও উন্নতি করেছে তাসকিন-এবাদতরা।
+		
+// 		এ জায়গাটায় তাসকিন-এবাদতসহ সব পেসারদের নিয়ে কাজ করবেন বলে জানিয়েছেন টাইগারদের সহকারী কোচ নিক পোথাস। তাসকিনের ব্যাটিং প্রশংসার পাশাপাশি তিনি বলেন, 'শুধু তাসকিন না, সবাইকেই ভালো ব্যাট করতে হবে। শেষদিকে যারা ব্যাটিংয়ে আসে তাদের জন্য ব্যাটিং করা কঠিন। আমরা তাদের সহায়তার চেষ্টা করছি। তাদের যতটা সম্ভব ভালো করা যায়। তাসকিন অনেক ভালো ব্যাটার। তার রেকর্ডও ভালো। বল মারতে পারে। সে দুর্দান্ত একজন ক্রিকেটার।'
+		
+// 		#TaskinAhmed | #NicPothas`,
+// 		post_media: {
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/277671575_116868387616818_3175016722292029734_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHc5NuYaMKQdSdOpxHqhbxbEuciOiiThvYS5yI6KJOG9htW9KluozRqauHAK-eWl5VelT-B9EN-u6Jyo48Y8KvL&_nc_ohc=DwzNsbieREEAX-T27B2&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfAmPauvtoRx8t-s2sC8CeKPtUzvZWA_kjRxMCZFEqYV4g&oe=64A4F3B4",
+// 		},
+
+// 		like_count: 1000,
+// 		comment_count: 1000,
+// 		post_time: "1 day",
+// 		user: {
+// 			name: "খেলার পাতা",
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/277671575_116868387616818_3175016722292029734_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHc5NuYaMKQdSdOpxHqhbxbEuciOiiThvYS5yI6KJOG9htW9KluozRqauHAK-eWl5VelT-B9EN-u6Jyo48Y8KvL&_nc_ohc=DwzNsbieREEAX-T27B2&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfAmPauvtoRx8t-s2sC8CeKPtUzvZWA_kjRxMCZFEqYV4g&oe=64A4F3B4",
+// 		},
+// 	},
+//     {
+// 		id: 4,
+// 		post_content: `অসাধারণ একটি ফুটবল ম্যাচ হয়েছে। নিকট অতীতে বাংলাদেশ দলের অন্যতম সেরা একটি ম্যাচ হয়েছে। শক্তিশালী দলের বিপক্ষে ভালো প্রস্তুতি ম্যাচ খেললে বাংলাদেশ দল আরও ভালো করতো বলে মনে করি। তারপরও দলের এমন লড়াকু পারফরম্যান্সে একজন সাধারণ ফুটবল সাপোর্টার হিসেবে খুশি। ফিটনেস ঘাটতির কারনেই বাংলাদেশ পিছিয়ে পড়েছে। এ জায়হায়টায় উন্নতি করতে হবে আমাদেরকে। ১২০ মিনিট যে দল জেতার জন্য খেলতে পারে সে দলকে নিয়ে ভরসা করাই যায়। এই সাফে বাংলাদেশ দল বেশ কিছু নতুন খেলোয়াড় পেয়েছে। তাদেরকে ভালো ভাবে নার্সিং করতে পারলে আগামীতে আরও ভালো কিছু হবে ইনশাআল্লাহ। শুভ কামনা রইলো বাংলাদেশ দলের জন্য।`,
+// 		post_media: {
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/357691970_6219526624839680_3996285912753597048_n.jpg?stp=dst-jpg_p180x540&_nc_cat=107&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeG7HVg_bHLdi4f8KbsHcicdhqcRlIGOsdqGpxGUgY6x2kJOXa4AM3ukT0aC-hSBu8FWnN9G9R34eQextn_BEs2S&_nc_ohc=jRUTfLbTpaAAX_K-Jc_&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfBlRw4JnXWhdY1Zd9muu1ZirgAprgqweEPFiyK4XdyGfw&oe=64A49B8D",
+// 		},
+
+// 		like_count: 600,
+// 		comment_count: 30,
+// 		post_time: "30 min",
+// 		user: {
+// 			name: "Md Shahadat Hossain",
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t1.6435-9/67610468_2232820096843706_6832607787508301824_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFm4tUrVEAqOBRZKx9KOZmm49T6wzA9zHTj1PrDMD3MdCwhs5Fh-tDZ5XV-5TGpTBuzErU33SWOPqC7AqfCgj7d&_nc_ohc=uNHke5xSknMAX__CFxW&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfBZ5G31MMtvBM7qVm_Tjg-TZcjujtssymEklYyXWdWVaQ&oe=64C7CE39",
+// 		},
+// 	},
+//     {
+// 		id: 5,
+// 		post_content: `অন্যের বেদনা দূর করার জন্য ব্যথা নেওযাই উদারতার আসল সারাংশ`,
+// 		post_media: {
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/356857077_1596034337551695_2520042062268784260_n.jpg?stp=dst-jpg_p526x296&_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHmVnkzyx8U4jdqp40hTSBVh3Rp8gBNSkGHdGnyAE1KQVJ-d6ZPlJPNc_6CFf6c7i3OSBDm-BNo7OMF3VbJovj1&_nc_ohc=yoJ6Pe1rxVoAX_ZnB_E&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfApnmdndzAIOmrdWKohCRmvIK4TqvsmcODnnr8bACtjfw&oe=64A597A7",
+// 		},
+
+// 		like_count: 10,
+// 		comment_count:2,
+// 		post_time: "1 min",
+// 		user: {
+// 			name: "Rasel Mahmud Foysal",
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/356857077_1596034337551695_2520042062268784260_n.jpg?stp=dst-jpg_p526x296&_nc_cat=101&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHmVnkzyx8U4jdqp40hTSBVh3Rp8gBNSkGHdGnyAE1KQVJ-d6ZPlJPNc_6CFf6c7i3OSBDm-BNo7OMF3VbJovj1&_nc_ohc=yoJ6Pe1rxVoAX_ZnB_E&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfApnmdndzAIOmrdWKohCRmvIK4TqvsmcODnnr8bACtjfw&oe=64A597A7",
+// 		},
+// 	},
+//     {
+// 		id: 6,
+// 		post_content: `We can do better boys 🙌🇧🇩`,
+// 		post_media: {
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/356221514_230483013170638_4510277896778160072_n.jpg?stp=dst-jpg_s640x640&_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGNRXIxgPW1pUo1T2lBd3Lvs3wpGs8PKlCzfCkazw8qUIf2byqvjtiZ_WP7-_8-Y1M4fqxafiPegzxMPyX2HK6D&_nc_ohc=jkFDCBi5Ds4AX_2BIwX&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfBXpum37FPQnVua4v33msbqD9XnoskldDb37_fHg5-I8g&oe=64A54296",
+// 		},
+
+// 		like_count: 1600,
+// 		comment_count: 100,
+// 		post_time: " 12 hr",
+// 		user: {
+// 			name: "Sports Talks",
+// 			photo:"https://www.facebook.com/photo/?fbid=230504089835197&set=a.107502145468726",
+// 		},
+// 	},
+//     {
+// 		id: 7,
+// 		post_content: `NaN`,
+// 		post_media: {
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/347604163_1005948250436122_399599486643334637_n.jpg?stp=c0.58.526.526a_dst-jpg_p526x296&_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGBKjSexpSMzJdLEk5IZtEFF1HMCl0PeQIXUcwKXQ95Alqap1URaMIF5q3xjuNxSN3-pM9Nj0x1uIEgqKyVTiEt&_nc_ohc=mg7wu_JE58wAX9ZZsta&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfAFjxiBbmXBf66yoKkOGZzw84y3JDmyfp4tEJxgmhRydQ&oe=64A55D87",
+// 		},
+
+// 		like_count: 20,
+// 		comment_count: 10,
+// 		post_time: " 2 hr",
+// 		user: {
+// 			name: "Tofik Aziz ",
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/347604163_1005948250436122_399599486643334637_n.jpg?stp=c0.58.526.526a_dst-jpg_p526x296&_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGBKjSexpSMzJdLEk5IZtEFF1HMCl0PeQIXUcwKXQ95Alqap1URaMIF5q3xjuNxSN3-pM9Nj0x1uIEgqKyVTiEt&_nc_ohc=mg7wu_JE58wAX9ZZsta&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfAFjxiBbmXBf66yoKkOGZzw84y3JDmyfp4tEJxgmhRydQ&oe=64A55D87",
+// 		},
+// 	},
+//     {
+// 		id: 8,
+// 		post_content: `সুড়ঙ্গ
+// 		A film by Raihan Rafi 🙏
+// 		— Shahriar Shakil-এর সাথে।`,
+// 		post_media: {
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/357162527_1927297734292077_176821238933794869_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFQqP8m06U8bEjHEcXMc4Io4t9IVn8DXtbi30hWfwNe1t_ilE7OWSxvVQiWBJIJ-cJRHoXoEsQKLyWa-zwlWUb2&_nc_ohc=ZXq_0ku7S1oAX86SRtl&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfCwf0os5cVS4zucKCin3KWzb_YbdRB5lg5z1PKhWpRm9g&oe=64A5187F",
+// 		},
+// 		like_count: 20,
+// 		comment_count: 10,
+// 		post_time: " 2 hr",
+// 		user: {
+// 			name: " Tama Mirza  ",
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/357162527_1927297734292077_176821238933794869_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFQqP8m06U8bEjHEcXMc4Io4t9IVn8DXtbi30hWfwNe1t_ilE7OWSxvVQiWBJIJ-cJRHoXoEsQKLyWa-zwlWUb2&_nc_ohc=ZXq_0ku7S1oAX86SRtl&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfCwf0os5cVS4zucKCin3KWzb_YbdRB5lg5z1PKhWpRm9g&oe=64A5187F",
+// 		},
+// 	},
+// 	{
+// 		id: 9,
+// 		post_content: `স্বরুপকাঠির ভাসমান পেয়ারা বাজার কবে থেকে শুরু হবে???`,
+// 		post_media: {
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/349186794_813135890177243_3098831695275606601_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeGWgxn7nF-6dNWLKLaEPBegEUR_6NaEmM0RRH_o1oSYzdYocpn72EqEpuwiQBRJn7yuYAUeB_CbRjpg1TN8A18y&_nc_ohc=sPhMzgbdp9IAX_B-UeQ&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfC27TXgRnqZAD4c6POxeC-7zOItenTPUU-3cLhVy2sjbQ&oe=64A655AC",
+// 		},
+// 		like_count: 20,
+// 		comment_count: 10,
+// 		post_time: " 2 hr",
+// 		user: {
+// 			name: "Travellers Of Bangladesh",
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/354985959_9457507027654444_7557818744515735702_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=8631f5&_nc_eui2=AeFUkxx_BtOwNN7KHuFsMfLUht95SrSh2IiG33lKtKHYiKzYzueo5GjcP_Wjd8Anypq215G7Hyk6n9MY7hBUCNuF&_nc_ohc=4niZknMveQMAX-lYhzc&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfBMfc7jxIGggPB_57_XXbGCUupFCYD7pjt-DWmQy6J33Q&oe=64A659FB",
+// 		},
+// 	},
+//     {
+// 		id: 10,
+// 		post_content: `সুড়ঙ্গ
+// 		A film by Raihan Rafi 🙏
+// 		— Shahriar Shakil-এর সাথে।`,
+// 		post_media: {
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/357162527_1927297734292077_176821238933794869_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFQqP8m06U8bEjHEcXMc4Io4t9IVn8DXtbi30hWfwNe1t_ilE7OWSxvVQiWBJIJ-cJRHoXoEsQKLyWa-zwlWUb2&_nc_ohc=ZXq_0ku7S1oAX86SRtl&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfCwf0os5cVS4zucKCin3KWzb_YbdRB5lg5z1PKhWpRm9g&oe=64A5187F",
+// 		},
+// 		like_count: 20,
+// 		comment_count: 10,
+// 		post_time: " 2 hr",
+// 		user: {
+// 			name: " Tama Mirza  ",
+// 			photo:"https://scontent.fcgp24-1.fna.fbcdn.net/v/t39.30808-6/357162527_1927297734292077_176821238933794869_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFQqP8m06U8bEjHEcXMc4Io4t9IVn8DXtbi30hWfwNe1t_ilE7OWSxvVQiWBJIJ-cJRHoXoEsQKLyWa-zwlWUb2&_nc_ohc=ZXq_0ku7S1oAX86SRtl&_nc_ht=scontent.fcgp24-1.fna&oh=00_AfCwf0os5cVS4zucKCin3KWzb_YbdRB5lg5z1PKhWpRm9g&oe=64A5187F",
+// 		},
+// 	},
+    
+// ]
